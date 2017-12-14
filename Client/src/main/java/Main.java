d3da331ba59c961d6ae21bda7d5be3823615c4e5
maksrojek <maksrojek@gmail.com>
2017-12-14 12:14:02 +0100

    Change in/out stream to send/receive SimpleMatrix object

--- Client/src/main/java/Main.java
+++ Client/src/main/java/Main.java
@@ -1,13 +1,29 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import org.ejml.simple.SimpleMatrix;
+import sun.java2d.pipe.SpanShapeRenderer;
+
+import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.Random;
 
 public class Main {
 
     static final int PORT_NUM = 4445;
+    static Random rand = new Random();
+
+    public static SimpleMatrix generateSquareMatrix(Integer dimension) {
+        return SimpleMatrix.random64(dimension, dimension, 1, 50, rand);
+    }
+
+    public static void serverResponse(SimpleMatrix A, SimpleMatrix B, SimpleMatrix resp) {
+        System.out.println("Input matrices:");
+        System.out.println("A:");
+        System.out.println(A);
+        System.out.println("B:");
+        System.out.println(B);
+        System.out.println("Server Response :");
+        System.out.println(resp);
+    }
 
     public static void main(String args[]) throws IOException {
 
@@ -15,14 +31,14 @@ public class Main {
         Socket s1 = null;
         String line = null;
         BufferedReader br = null;
-        BufferedReader is = null;
-        PrintWriter os = null;
+        ObjectOutputStream oos = null;
+        ObjectInputStream ois = null;
 
         try {
             s1 = new Socket(address, PORT_NUM);
             br = new BufferedReader(new InputStreamReader(System.in));
-            is = new BufferedReader(new InputStreamReader(s1.getInputStream()));
-            os = new PrintWriter(s1.getOutputStream());
+            ois = new ObjectInputStream(s1.getInputStream());
+            oos = new ObjectOutputStream(s1.getOutputStream());
         } catch (IOException e) {
             e.printStackTrace();
             System.err.print("IO Exception");
@@ -31,25 +47,32 @@ public class Main {
         System.out.println("Client Address : " + address);
         System.out.println("Enter Data to echo Server ( Enter QUIT to end):");
 
-        String response = null;
+        SimpleMatrix response = null;
         try {
             line = br.readLine();
+            //TODO: check if 'line' is number or "QUIT", refuse others
+
             while (line.compareTo("QUIT") != 0) {
-                os.println(line);
-                os.flush();
-                response = is.readLine();
-                System.out.println("Server Response : " + response);
+                //TODO: generate nxm and mxk matrices to increase complexity
+                //generate random square matrices based on input
+                SimpleMatrix A = generateSquareMatrix(Integer.parseInt(line));
+                SimpleMatrix B = generateSquareMatrix(Integer.parseInt(line));
+
+                oos.writeObject(A);
+                oos.writeObject(B);
+                oos.flush();
+                response = (SimpleMatrix) ois.readObject();
+                serverResponse(A, B, response);
                 line = br.readLine();
             }
 
-
-        } catch (IOException e) {
+        } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
             System.out.println("Socket read Error");
         } finally {
 
-            is.close();
-            os.close();
+            ois.close();
+            oos.close();
             br.close();
             s1.close();
             System.out.println("Connection Closed");
@@ -57,4 +80,6 @@ public class Main {
         }
 
     }
+
+
 }
