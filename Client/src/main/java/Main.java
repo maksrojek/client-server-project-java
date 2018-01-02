669d1461be533f61eda9e04cd8a1b8f049bc7762
maksrojek <maksrojek@gmail.com>
2018-01-02 15:26:58 +0100

    Replace SimpleMatrix with DMatrixRMaj
    SimpleMatrix generates not serializable exception when sending trough
    socket, DMatrixRMaj doesn't.

--- Client/src/main/java/Main.java
+++ Client/src/main/java/Main.java
@@ -1,3 +1,5 @@
+import org.ejml.data.DMatrixRMaj;
+import org.ejml.dense.row.RandomMatrices_DDRM;
 import org.ejml.simple.SimpleMatrix;
 import sun.java2d.pipe.SpanShapeRenderer;
 
@@ -11,12 +13,13 @@ public class Main {
     static final int PORT_NUM = 4445;
     static Random rand = new Random();
 
-    public static SimpleMatrix generateSquareMatrix(Integer dimension) {
-        return SimpleMatrix.random64(dimension, dimension, 1, 50, rand);
+    public static DMatrixRMaj generateRandomSquareMatrix(int dimension, double
+            minValue, double maxValue) {
+        return RandomMatrices_DDRM.symmetric(dimension, minValue, maxValue, rand);
     }
 
-    public static void serverResponse(SimpleMatrix A, SimpleMatrix B, SimpleMatrix resp) {
-        System.out.println("Input matrices:");
+    public static void printServerResponse(Object A, Object B, Object resp) {
+        System.out.println("Input:");
         System.out.println("A:");
         System.out.println(A);
         System.out.println("B:");
@@ -29,7 +32,6 @@ public class Main {
 
         InetAddress address = InetAddress.getLocalHost();
         Socket s1 = null;
-        String line = null;
         BufferedReader br = null;
         ObjectOutputStream oos = null;
         ObjectInputStream ois = null;
@@ -37,8 +39,8 @@ public class Main {
         try {
             s1 = new Socket(address, PORT_NUM);
             br = new BufferedReader(new InputStreamReader(System.in));
-            ois = new ObjectInputStream(s1.getInputStream());
             oos = new ObjectOutputStream(s1.getOutputStream());
+            ois = new ObjectInputStream(s1.getInputStream());
         } catch (IOException e) {
             e.printStackTrace();
             System.err.print("IO Exception");
@@ -47,22 +49,23 @@ public class Main {
         System.out.println("Client Address : " + address);
         System.out.println("Enter Data to echo Server ( Enter QUIT to end):");
 
-        SimpleMatrix response = null;
+        String line;
+        DMatrixRMaj A, B, response;
         try {
             line = br.readLine();
             //TODO: check if 'line' is number (>0) or "QUIT", refuse others
 
             while (line.compareTo("QUIT") != 0) {
-                //TODO: generate nxm and mxk matrices to increase complexity
-                //generate random square matrices based on input
-                SimpleMatrix A = generateSquareMatrix(Integer.parseInt(line));
-                SimpleMatrix B = generateSquareMatrix(Integer.parseInt(line));
+                //TODO:import or generate nxm, mxk matrices to increase complexity
+                //generating random square matrices based on input
+                A = generateRandomSquareMatrix(Integer.parseInt(line), 1, 50);
+                B = generateRandomSquareMatrix(Integer.parseInt(line), 1, 50);
 
                 oos.writeObject(A);
                 oos.writeObject(B);
                 oos.flush();
-                response = (SimpleMatrix) ois.readObject();
-                serverResponse(A, B, response);
+                response = (DMatrixRMaj) ois.readObject();
+                printServerResponse(A, B, response);
                 line = br.readLine();
             }
 
